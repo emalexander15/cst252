52b0216c4f5fe7e8fb0f37a7aac79065be452909
emalexander15 <emalexander@csumb.edu>
2021-09-12 21:00:29 -0700

    Lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,43 @@
+/**
+ * Summary. (use period)
+ *
+ * Description. (use period)
+ *
+ * @link   URL
+ * @file   This files defines the MyClass class.
+ * @author AuthorName.
+ * @since  x.x.x
+ */
+
+/** jshint {inline configuration here} */
+
+// Think of a subject you would want as an icon to your folders
+// Create your own png in an Adobe software app such as Photoshop or a website such as Canva
+  // Launch Adobe Photoshop
+    // Import a file of your choosing
+    // Create a customized logo
+        // Save as png
+  // Launch Canva
+    // Type in the item you are looking for
+      // Drag item on to blank page
+        // Save as png
+// If you are unable to do the previous step, look up "(your subject) png" in your preferred search browser
+  // Launch internet search software such as Google Chrome
+    // Type in preferred item in searchbar ending in "png"
+// Check that your file is a png with no background
+  // When dragging item check there is no background color or transparency
+// Drag the png you would like to change it to to your desktop and choose Edit > Copy
+  // Click file and drag on to desktop
+    // Navigate to the menu bar and choose Edit
+      // After choosing Edit choose Copy
+// Click the designated folder on your desktop you would like to change from Apple's given blue folder icon to the icon of your choice
+  // Click with mouse
+// Right click the folder and select File > Get Info
+  // Navigate to the menu bar and choose File
+    // After choosing File choose Get Info
+// Once on the file preview, click the small icon in the top left corner and choose Edit > Paste
+  // Move your mouse over the small icon
+    // Navigate to the menu bar and choose Edit
+      // After selecting Edit choose Paste
+        // Selection will be pasted on top of previous folder icon 
+// Repeat steps if you would like to do it more than once
