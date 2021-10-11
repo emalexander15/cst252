ed4fb643c5efef957293cb276060ae53dc7aae2c
emalexander15 <emalexander@csumb.edu>
2021-10-11 00:01:20 -0700

    Lab 11

---
+++ lab11/js/lab.js
@@ -0,0 +1,15 @@
+/**
+ * Author:    Emma Alexander <emalexander@csumb.edu>
+ * Created:   10.10.2021
+ *
+ * License: Public Domain
+ **/
+
+ // create a <button> element
+ $(document).ready(function () {
+    $("#btn").click(function () {
+      alert("You clearly get bossed around.");
+    });
+ });
+
+ $("#btn").css("background", "lightgreen")
