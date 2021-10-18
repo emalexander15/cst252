5f17b3c9dd475a821d4de1f8138ec5e519a3a92b
emalexander15 <emalexander@csumb.edu>
2021-10-18 01:13:26 -0700

    Lab 12

---
+++ lab12/js/lab.js
@@ -0,0 +1,18 @@
+/**
+ * Author:    Emma Alexander <emalexander@csumb.edu>
+ * Created:   10.18.2021
+ *
+ * License: Public Domain
+ **/
+
+$("#challenge-btn").click(function (){
+  $("#challenges").toggleClass("beigetext");
+});
+
+$("#problems-btn").click(function (){
+  $("#problems").toggleClass("big");
+});
+
+$("#results-btn").click(function (){
+  $("#results").toggleClass("highlight");
+});
