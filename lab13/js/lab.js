3972504cbfbd7befdf488d1e6d4575fc99f4338b
emalexander15 <emalexander@csumb.edu>
2021-10-19 18:57:11 -0700

    Bratz Lab 13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -11,16 +11,16 @@
    len = str.length;
    mod = len % 4;
    if (mod == 0) {
-     return "Gryffindor"
+     return "Yasmin (4th)"
    }
  else if (mod == 1) {
-   return "Ravenclaw"
+   return "Cloe (2nd)"
  }
  else if (mod == 2) {
-   return "Slytherin"
+   return "Jade (3rd)"
  }
  else if (mod == 3) {
-   return "Hufflepuff"
+   return "Sasha (1st)"
  }
 }
 
@@ -28,6 +28,6 @@
  myButton.addEventListener("click", function () {
    var name = document.getElementById("input").value;
    var house = sortingHat(name);
-   newText = "<p>The sorting hat has sorted you into " + house + "</p>";
+   newText = "<p>The sorting hat has said you are: " + house + "</p>";
    document.getElementById("output").innerHTML = newText;
  })
