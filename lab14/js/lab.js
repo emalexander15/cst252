fadf6ca78bc685f0db7cadb9b3166b7259fcf1cc
emalexander15 <emalexander@csumb.edu>
2021-10-25 18:53:06 -0700

    Lab 14

--- lab14/js/lab.js
+++ lab14/js/lab.js
@@ -1,46 +1,45 @@
 /**
  * Author:    Emma Alexander <emalexander@csumb.edu>
- * Created:   10.20.2021
+ * Created:   10.25.2021
  *
  * License: Public Domain
  **/
 
-for (var num=1; num <= 200; num++) {
-var outputStr = ""
+ for (i=1; i <= 200; i++) {
+   var outputEl = document.getElementById("output") ;
+   var writeNumber = document.createElement("p");
 
-// if multiple of 3 add Fizz
-if (num % 3 == 0) {
-	outputStr = outputStr += "Fizz"
-}
-
-str += "Fizz";
-$("#output").append("<p>" + str);
-});
-
-// if multiple of 5 add Buzz
-if (num % 5 == 0) {
-	outputStr = outputStr += "Buzz"
-}
-
-str += "Buzz";
-$("#output").append("<p>" + str);
-});
-
-// if multiple of 7 add Boom
-if (num % 7 == 0) {
-	outputStr = outputStr += "Boom"
-}
-
-str += "Boom";
-$("#output").append("<p>" + str);
-});
-
-// if string is still blank, print it
-if (outputStr == "") {
-	console.log(num);
-}
-// otherwise, print str
-else {
-	console.log(outputStr + '!');
-}
+   if (i%105 == 0) {
+     writeNumber.innerHTML = "FizzBuzzBoom";
+     outputEl.appendChild(writeNumber)
+     console.log("FizzBuzzBoom");
+   } else if (i%35 == 0) {
+     writeNumber.innerHTML = "BuzzBoom";
+     outputEl.appendChild(writeNumber)
+     console.log("BuzzBoom");
+   } else if (i%21 == 0) {
+     writeNumber.innerHTML="FizzBoom";
+     outputEl.appendChild(writeNumber)
+     console.log ("FizzBoom");
+   } else if (i%15 == 0) {
+     writeNumber.innerHTML="FizzBuzz"
+     outputEl.appendChild(writeNumber)
+     console.log ("FizzBuzz");
+   } else if (i%3 == 0) {
+     writeNumber.innerHTML = "FizzBuzz";
+     outputEl.appendChild(writeNumber)
+     console.log("Fizz");
+   } else if (i%5 == 0) {
+     writeNumber.innerHTML = "Buzz";
+     outputEl.appendChild(writeNumber)
+     console.log("Buzz");
+   } else if (i%7 == 0) {
+     writeNumber.innerHTML = "Boom";
+     outputEl.appendChild(writeNumber)
+     console.log("Boom");
+   } else {
+     writeNumber.innerHTML = i;
+     outputEl.appendChild(writeNumber)
+     console.log(i);
+   }
 }
