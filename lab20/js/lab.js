16822ecc21c0238688b6c8ded802dd598cff95ed
emalexander15 <emalexander@csumb.edu>
2021-12-13 20:33:49 -0800

    Lab 20

--- lab20/js/lab.js
+++ lab20/js/lab.js
@@ -6,35 +6,27 @@
  **/
 
  // Pick an URL from API
- var targetUrl = "https://thecocktaildb.com/api/json/v1/1/random.php";
+ URL = "https://coffee.alexflipnote.dev/random";
 
- function doAjaxStuff() {
-  // Using the core $.ajax() method
+ // Click button
+ $('#activate').click(getimg);
+
+ function getimg() {
    $.ajax({
-       // The URL for the request
-       url: targetUrl,
-       // The data to send (will be converted to a query string)
-       // data: { id: 123},
-       // Whether this is a POST or GET request
-       type: "GET",
-       // The type of data we expect back
-       // dataType : "json",
-   })
-   // If the request succeeds
-   .done(function( data ) {
-       alert("Success!");
-       console.log(data);
-       $("#output").html("<pre>" + JSON.stringify(data, null, 2) + "</pre>");
-   })
-   // If the request fails
-   .fail(function( xhr, status, errorThrown ) {
-       console.log(errorThrown + " Status:" + status );
-       alert("Failure");
-   });
+     // endpoint
+     ur: URL,
+     // type
+     type: "GET",
+ })
 
- }
+ // if succeeds
+ .done(function(data) {
+   console.log(data);
+   var imageUrl = data.image;
+   var imageHtml = "<img src='" + imageURL + "'>";
+   console.log(imageUrl);
+   $("#output").html(imageHtml);
+ })
+}
 
- // When a user clicks the button, use a jQuery AJAX call to fetch output from the numbers API
- $("#activate").click(doAjaxStuff)
-
- // Insert the output in the output div
+getimg()
