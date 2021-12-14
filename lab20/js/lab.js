1970d07f0244a4360e9d88ca2fec4a95b69b4ac1
emalexander15 <emalexander@csumb.edu>
2021-12-13 20:23:44 -0800

    lab 19 and 20

---
+++ lab20/js/lab.js
@@ -0,0 +1,40 @@
+/**
+ * Author:    Emma Alexander <emalexander@csumb.edu>
+ * Created:   11.18.2021
+ *
+ * License: Public Domain
+ **/
+
+ // Pick an URL from API
+ var targetUrl = "https://thecocktaildb.com/api/json/v1/1/random.php";
+
+ function doAjaxStuff() {
+  // Using the core $.ajax() method
+   $.ajax({
+       // The URL for the request
+       url: targetUrl,
+       // The data to send (will be converted to a query string)
+       // data: { id: 123},
+       // Whether this is a POST or GET request
+       type: "GET",
+       // The type of data we expect back
+       // dataType : "json",
+   })
+   // If the request succeeds
+   .done(function( data ) {
+       alert("Success!");
+       console.log(data);
+       $("#output").html("<pre>" + JSON.stringify(data, null, 2) + "</pre>");
+   })
+   // If the request fails
+   .fail(function( xhr, status, errorThrown ) {
+       console.log(errorThrown + " Status:" + status );
+       alert("Failure");
+   });
+
+ }
+
+ // When a user clicks the button, use a jQuery AJAX call to fetch output from the numbers API
+ $("#activate").click(doAjaxStuff)
+
+ // Insert the output in the output div
